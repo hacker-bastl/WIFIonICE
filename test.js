bee3242f18bc46a83277eb5e4fce2652aed15001
hacker-bastl <hacker-bastl@users.noreply.github.com>
2017-07-13 10:00:48 +0200

    nodejs for testing locally

--- test.js
+++
@@ -1,6 +0,0 @@
-// https://github.com/expressjs/serve-static#simple
-const express = require('express');
-const static = require('serve-static');
-const application = express();
-application.use(static('docs'));
-application.listen(process.env.PORT || 80);
